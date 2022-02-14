513eb22fab2c7cfb9ef2fc1f88a1fc679f3cf6d5
k2Forest-Man <lesnoichelovek@yandex.ru>
2022-02-15 00:38:13 +0300

    прописал все стили для адаптива, добавил событие на кнопку видео, кнопку открытия-закрытия меню на мобиле

--- js/main.js
+++ js/main.js
@@ -1,8 +1,43 @@
 const swiper = new Swiper('.main-slider', { // контейнер в котором находится наш слайдер
   loop: true,                       // <--- зацикленность
-  slidesPerView : 2,                 // Добавляем количество отображаемых слайдов
   spaceBetween: 30,                  // Отступ между слайдами
   navigation: {                     // Navigation arrows
     nextEl: '.switch-slider',  // Кнопка управления слайдером, только вперед!!!
   },
+  breakpoints: {
+    320: {
+      slidesPerView : 1,
+    },
+    768: {
+      slidesPerView : 2,      // Добавляем количество отображаемых слайдов
+    },
+  }
+});
+
+let burger = document.querySelector ('.menu-burger');
+let closeMenu = document.querySelector ('.button-close');
+let menuMobile = document.querySelector ('.main-header');
+
+
+burger.addEventListener('click', () => {
+  menuMobile.style.display = 'block';
+})
+
+closeMenu.addEventListener('click', () => {
+  menuMobile.style.display = 'none';
+})
+
+const modalWindow = document.querySelector('.modal');
+const buttonModal = document.querySelector('.button-play');
+
+buttonModal.addEventListener ('click', (e) =>{
+  modalWindow.classList.add ('active');
+});
+
+modalWindow.addEventListener ('click', (e) =>{
+  const isModal = e.target.closest ('.modal-inner') // ловим modal-inner, чтобы закрыть видео
+
+  if(!isModal) {      //Если НЕ ИЗ Modal
+    modalWindow.classList.remove('active');
+  }
 });
