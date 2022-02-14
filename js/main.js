7a59b843e272ba519b2e794ad8af369355d63ff4
k2Forest-Man <lesnoichelovek@yandex.ru>
2022-02-14 16:40:03 +0300

    добавил сетку медиазапросов, убрал отступы всем заголовкам

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper('.main-slider', { // контейнер в котором находится наш слайдер
   loop: true,                       // <--- зацикленность
   slidesPerView : 2,                 // Добавляем количество отображаемых слайдов
-  spaceBetween: 30,
+  spaceBetween: 30,                  // Отступ между слайдами
   navigation: {                     // Navigation arrows
     nextEl: '.switch-slider',  // Кнопка управления слайдером, только вперед!!!
   },
